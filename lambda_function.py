e7a8bc53d3930448c0ee88385379816abc0321a8
Marlon Gracietti de Amorim <marlon.gracietti@levva.io>
2024-03-25 13:48:50 -0300

    add user into agroup

--- lambda_function.py
+++ lambda_function.py
@@ -13,9 +13,23 @@ def lambda_handler(event, context):
         print("Authorization header is missing")
         return generate_policy("user", "Deny", event["methodArn"])
 
-    encoded_token = auth_header.split(" ")[1]
-    decoded_value = base64.b64decode(encoded_token).decode("utf-8")    
-    username, password = decoded_value.split(":")
+    print("auth_header: ", auth_header)
+    
+    auth_header = auth_header.replace("Basic", "").strip()
+    auth_header_no_spaces = auth_header.replace(" ", "")
+    
+    
+    try:
+        decoded_value = base64.b64decode(auth_header_no_spaces).decode("utf-8")
+    except Exception as e:
+        print(f"Error decoding authorization token: {e}")
+        return generate_policy("user", "Deny", event["methodArn"])
+
+    username, password = decoded_value.split(":") 
+
+    #encoded_token = auth_header.split(" ")[1]
+    #decoded_value = base64.b64decode(encoded_token).decode("utf-8")    
+    #username, password = decoded_value.split(":")
     
     try:
         user_attributes = get_user_attributes(username, USER_POOL_ID)
